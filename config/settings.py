f54022456c9e123920e3bce3c5dd3ce916313a68
EricVaLo <erickjosevalenzuelalopez@gmail.com>
2026-09-15 13:20:55 -0500

    ultima

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# WhiteNoise solo en producción (Render)
+if os.environ.get('RENDER'):
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
